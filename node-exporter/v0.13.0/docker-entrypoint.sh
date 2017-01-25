d0594375fd7de3b1026eee463fdbb98458483d71
Aldo Bongio <aldo.bongio@webratio.com>
2017-01-25 17:04:08 +0100

    Added mkdir for crating prom static metric directory

--- node-exporter/v0.13.0/docker-entrypoint.sh
+++ node-exporter/v0.13.0/docker-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+mkdir -p /etc/node-exporter/
 echo "docker_node{id=\"$DOCKER_NODE_ID\",hostname=\"$DOCKER_NODE_HOSTNAME\"} 1" > /etc/node-exporter/host_hostname.prom
 
 set -- $NODE_EXPORTER_BIN "$@"
