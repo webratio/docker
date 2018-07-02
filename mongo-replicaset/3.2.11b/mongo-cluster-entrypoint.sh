d12bd43572b90a99e5b185b2f715c901c871f549
Aldo Bongio <aldo.bongio@webratio.com>
2018-07-02 17:20:47 +0200

    Improved mongo startup script

--- mongo-replicaset/3.2.11b/mongo-cluster-entrypoint.sh
+++ mongo-replicaset/3.2.11b/mongo-cluster-entrypoint.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-set -x
-echo "1: $1"
-echo "2: $2"
-echo "3: $3"
-echo "all: $@"
+set -e
 
-echo "Sleeping 20 seconds..."
+echo "Waiting 20 seconds for Docker swarm network name..."
 sleep 20
-echo "Sleep done"
 /entrypoint.sh "$@"
